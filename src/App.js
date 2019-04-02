4081a711ef3eae5da911f44bda2bf107910efcd6
Your Name <github email address>
2019-04-02 17:31:57 -0400

    some homepage styling

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import { connect } from 'react-redux'
 import { Redirect } from 'react-router'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import wheelGif from './images/wheel-gif.gif'
 
 import Nav from './components/Nav'
 import Login from './components/Login'
@@ -15,6 +16,12 @@ class App extends Component {
       <Router>
       <div className="App">
         <Nav />
+        <span>
+          <img className="wheel-gif" src={wheelGif} alt="spinny wheel" />
+        </span>
+        <span>
+          <h1 className="logo">CYCLE SOURCE</h1>
+        </span>
         {this.props.loginClicked ? <Login /> : null}
         {this.props.newUser ? <SignUp /> : null}
       </div>
